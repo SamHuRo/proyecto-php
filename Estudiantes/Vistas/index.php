7d6e60c7d255f5846efc7967a05fab6e30de7c5d
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2024-09-09 16:47:28 -0500

    Configuration of Estudiantes

--- Estudiantes/Vistas/index.php
+++ Estudiantes/Vistas/index.php
@@ -2,18 +2,40 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="C:\xampp\htdocs\diplomado\proyecto-php\style.css">
+    <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Notas</title>
 </head>
 <body>
-    <div class="contenedor">
-        <h1>Estudiantes</h1>
-    </div>
 
-    <div class="contenedor">
+
+    
+    
+    <div class="conteiner">
+        <div>
+            <button><a href="add.php">Registrar Estudiante</a></button>
+        </div>
+        <br><br>
         <table>
-
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Nombre</th>
+                    <th>Apellido</th>
+                    <th>Documento</th>
+                    <th>Correo</th>
+                    <th>Materias</th>
+                    <th>Docente</th>
+                    <th>Promedio</th>
+                    <th>Fecha de Registro</th>
+                    <th>Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Hola</td>
+                </tr>
+            </tbody>
         </table>
     </div>
 
