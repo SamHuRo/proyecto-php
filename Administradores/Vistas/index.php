f5289b23be3da2f9e59c023ae2233f3704310f6d
SHuertasRo <88412486+SamHuRo@users.noreply.github.com>
2024-09-16 16:06:52 -0500

    update

--- Administradores/Vistas/index.php
+++ Administradores/Vistas/index.php
@@ -4,16 +4,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../../Styles/style_table.css">
+    <link rel="stylesheet" href="../../Estudiantes/style_table.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Notas</title>
 </head>
 <body>
-     
+    <header>
+            <div class="menu">
+                <?php
+                    $directorio_actual = getcwd();
+
+                    $directorio_padre = dirname($directorio_actual);
+                    $nombre_carpeta_padre = basename($directorio_padre);
+                ?>
+
+                <h1><?php echo $nombre_carpeta_padre;?></h1>            
+                <nav>
+                    <ul>
+                        <li><a class="navegador" href="../../Estudiantes/Vistas/index.php">Estudiantes</a></li>
+                        <li><a class="navegador" href="../../Docentes/Vistas/index.php">Docentes</a></li>
+                        <li><a class="navegador" href="../../Materias/Vistas/index.php">Materias</a></li>
+                        <li><a class="navegador" href="../../Usuarios/Controladores/logout.php">Cerrar Sesión</a></li>
+                    </ul>
+                </nav>
+                
+            </div>
+    </header> <br>
     <div class="conteiner">
-        <div>
-            <h1>Administradores</h1>
-        </div> <br><br>
+
         <div>
             <button><a href="add.php" target="_blank">Registrar Administrador</a></button>
         </div>
--- Materias/Vistas/index.php
+++ Materias/Vistas/index.php
@@ -1,8 +1,6 @@
 <?php
 require_once('../../Usuarios/Modelo/ususarios.php');
 
-session_start();
-
 $Modelo = new Usuario();
 $Modelo->validateSession();
 
@@ -12,11 +10,32 @@ $Modelo->validateSession();
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <link rel="stylesheet" href="../../Styles/style_table.css">
+    <link rel="stylesheet" href="../../Estudiantes/style_table.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema de Notas</title>
 </head>
 <body>
+    <header>
+        <div class="menu">
+            <?php
+                $directorio_actual = getcwd();
+
+                $directorio_padre = dirname($directorio_actual);
+                $nombre_carpeta_padre = basename($directorio_padre);
+            ?>
+
+            <h1><?php echo $nombre_carpeta_padre;?></h1>
+            <nav>
+                <ul>
+                    <li><a class="navegador" href="../../Administradores/Vistas/index.php">Administradores</a></li>
+                    <li><a class="navegador" href="../../Estudiantes/Vistas/index.php">Estudiantes</a></li>
+                    <li><a class="navegador" href="../../Docentes/Vistas/index.php">Docentes</a></li>
+                    <li><a class="navegador" href="../../Usuarios/Controladores/logout.php">Cerrar Sesión</a></li>
+                </ul>
+            </nav>
+            
+        </div>
+    </header> <br>
      
     <div class="conteiner">
         <div>
